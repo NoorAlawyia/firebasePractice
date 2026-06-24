0b75394d3f0c28766b38f263ee9d55f7061d5660
noor <noorahmed123yellow@gmail.com>
2026-06-24 09:55:39 +0000

    new files added

---
+++ src/HotPicks.jsx
@@ -0,0 +1,9 @@
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { useState, useEffect} from 'react'
+import { db } from "./firebaseConfig"
+import { doc, getDoc} from "firebase/firestore"
+
+return( 
+    <div className="hotpicks-carousel"> 
+    </div>
+)
